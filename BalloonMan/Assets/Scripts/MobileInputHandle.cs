d1a6fde4e091856d22443834909e77e3f2ec3d43
bailu <bailu@meetvr.com.cn>
2017-04-21 16:12:02 +0800

    加入官方的插件和脚本

---
+++ BalloonMan/Assets/Scripts/MobileInputHandle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInputHandle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(Input.GetTouch(0).)
+	}
+}
